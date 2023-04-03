8c6d42db5128762e54d7cfcd40678aa9b757a51b
Mounika <129516803+MounikaAnyam@users.noreply.github.com>
2023-04-03 20:06:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,71 +1,97 @@
-const todosUL = document.getElementById('todos')
-const form = document.getElementById('form')
-const input = document.querySelector('#input')
+const quizzData=[
+    {
+        question:"Markup Language?",
+        a:"Html",
+        b:"Css",
+        c:"Js",
+        d:"Python",
+        correct:"a"
+    },
+    {
+        question:"Style sheet?",
+        a:"Html",
+        b:"Css",
+        c:"Js",
+        d:"Python",
+        correct:"b"
+    },
+    {
+        question:"Simple Syntax?",
+        a:"C++",
+        b:"Java",
+        c:"Js",
+        d:"Python",
 
-const todos = JSON.parse(localStorage.getItem('todos'))
+        correct:"d"
+    },
+    {
+        question:"Backend Language?",
+        a:"Html",
+        b:"Css",
+        c:"Js",
+        d:"Python",
+        correct:"d"
+    },
+];
+const quizz = document.getElementById('quiz');
+const a_text = document.getElementById('a_text');
+const  b_text = document.getElementById('b_text');
+const  c_text = document.getElementById('c_text');
+const  d_text = document.getElementById('d_text');
+const answerEls=document.querySelectorAll(".answer");
+const questionEl=document.getElementById("question");
+const submitBtn=document.getElementById("submit");
 
-if (todos) {
-    todos.forEach(todo => addToDo(todo))
+
+let currentQuiz = 0
+let score = 0
+loadQuiz()
+
+function loadQuiz() {
+    deselectAnswer()
+    const currentQuizData=quizzData[currentQuiz]
+    questionEl.innerText=currentQuizData.question
+    a_text.innerText=currentQuizData.a
+    b_text.innerText=currentQuizData.b
+    c_text.innerText=currentQuizData.c
+    d_text.innerText=currentQuizData.d
+
+
 }
+function deselectAnswer(){
+    answerEls.forEach(answerEl=>answerEl.checked = false)
+}
+function getSelected(){
+    let answer
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-
-    addToDo()
-})
-function addToDo(todo) {
-    let todoText = input.value;
-    if (todo) {
-        todoText = todo.text
-    }
-
-    if (todoText) {
-        const todoEl = document.createElement('li')
-        if (todo && todo.completed) {
-            todoEl.classList.add("completed")
+    answerEls.forEach(answerEl=> {
+        if(answerEl.checked){
+            answer = answerEl.id
         }
-        todoEl.innerText = todoText;
-
-
-        todoEl.addEventListener('click', () => {
-            todoEl.classList.toggle('completed')
-            updateLS()
-
-        })
-
-        todoEl.addEventListener('contextmenu', (e) => {
-
-            e.preventDefault()
-            todoEl.remove()
-            updateLS()
-
-        })
-
-        todosUL.appendChild(todoEl)
-
-        console.log(todoEl)
-        input.value = ''
-
-        updateLS()
-
-    }
-}
-
-
-function updateLS() {
-    todosEl = document.querySelectorAll("li")
-    const todos = []
-    todosEl.forEach(todoEl => {
-        todos.push({
-            text: todoEl.innerText,
-            completed:todoEl.classList.contains('completed')
-        })
     })
-
-
-
-
-    localStorage.setItem("todos", JSON.stringify(todos))
-
-
+    return answer
 }
+submitBtn.addEventListener("click",()=>{
+    const answer = getSelected()
+    if(answer) {
+        if(answer === quizzData[currentQuiz].correct){
+            score++
+        }
+        currentQuiz++
+        if(currentQuiz < quizzData.length) {
+            loadQuiz()   
+        } else {
+            quizz.innerHTML = `
+            <h2>You answered correctly at  ${score}/${quizzData.length} question correctly</h2>
+
+             <button onclick="location.reload()">Reload</button>`         
+            
+        }
+         
+    
+
+        }
+    })
+    
+    
+
